58166409b9e2dc51dd709fec2c440ab69900f524
talhacheema675 <125282171+talhacheema675@users.noreply.github.com>
2023-08-20 21:03:35 +0500

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Ueffect from '@/components/quiz'
 export default function Home() {
   return ( 
-    <div className='bg-blue-500 text-black h-full w-full p-10'> 
+    <div className='bg-blue-500 text-black h-full w-full md:p-10 p-2 '> 
     <Ueffect/>
     
     </div>
--- src/components/quiz.tsx
+++ src/components/quiz.tsx
@@ -27,24 +27,24 @@ const Ueffect=()=>{
     );
 return(
    <div>
-    <div className={`${value==39?"invisible":"bg-gray-500 p-4 md:w-2/3 w-fit rounded-3xl h-max md:mx-64 md:my-32 m-4 flex md:flex-row flex-col "}`}>
+    <div className={`${value==39?"invisible":"bg-gray-500 p-4 md:w-2/3 w-96 rounded-3xl h-max md:mx-64 md:my-32 m-4 flex md:flex-row flex-col "}`}>
                 <div className="w-2/3 flex flex-col">
                     <h1 className="text-3xl font-bold">Question {value +1}/40</h1>
-                    <label className="text-center text-2xl md:h-44 h-12 ">{mydata?.results[value].question}</label>
-                    <button onClick={()=> {setvalue(value-value); setRight(right-right); setWrong(wrong-wrong)}} className="bg-green-600 text-2xl font-bold w-44 h-10 mt-10 ml-10 md:visible invisible">Start again</button>
+                    <label className="text-center md:text-2xl text-lg md:h-44 h-24 ">{mydata?.results[value].question}</label>
+                    <div className="md:visible invisible"><button onClick={()=> {setvalue(value-value); setRight(right-right); setWrong(wrong-wrong)}} className={`${value!=39?"bg-green-600 text-2xl font-bold w-44 h-10 mt-10 ml-10": "invisible"}`}>Start again</button></div>
                    
                 </div>
-                <div className="pt-6 text-xl flex flex-col text-left"> 
+                <div className="md:pt-6  md:text-xl text-lg flex flex-col text-left pl-6 "> 
                     
-                        <button onClick={()=>{setWrong(wrong+1); setvalue(value+1) }} className="border-2 border-solid w-64 h-16 border-black rounded-2xl pt-2">{mydata?.results[value].incorrect_answers[0]}</button>
-                        <button onClick={()=>{setWrong(wrong+1); setvalue(value+1) }} className="border-2 border-solid w-64 h-16 border-black rounded-2xl pt-1">{mydata?.results[value].incorrect_answers[1]}</button>
-                        <button onClick={()=>{setWrong(wrong+1); setvalue(value+1) }} className="border-2 border-solid w-64 h-16 border-black rounded-2xl pt-1">{mydata?.results[value].incorrect_answers[2]}</button>
-                        <button onClick={()=>{setRight(right+1); setvalue(value+1) }} className="border-2 border-solid w-64 h-16 border-black rounded-2xl pb-2">{mydata?.results[value].correct_answer}</button>
+                        <button onClick={()=>{setWrong(wrong+1); setvalue(value+1) }} className="border-2 border-solid w-64 h-16 border-black rounded-2xl mt-2">{mydata?.results[value].incorrect_answers[0]}</button>
+                        <button onClick={()=>{setWrong(wrong+1); setvalue(value+1) }} className="border-2 border-solid w-64 h-16 border-black rounded-2xl mt-1">{mydata?.results[value].incorrect_answers[1]}</button>
+                        <button onClick={()=>{setWrong(wrong+1); setvalue(value+1) }} className="border-2 border-solid w-64 h-16 border-black rounded-2xl my-1 ">{mydata?.results[value].incorrect_answers[2]}</button>
+                        <button onClick={()=>{setRight(right+1); setvalue(value+1) }} className="border-2 border-solid w-64 h-16 border-black rounded-2xl mb-2">{mydata?.results[value].correct_answer}</button>
                 </div>
-                <button onClick={()=> {setvalue(value-value); setRight(right-right); setWrong(wrong-wrong)}} className="bg-green-600 text-2xl font-bold w-44 h-10 mt-10 ml-10 md:invisible visible">Start again</button>
+                <div className="md:invisible visible"><button onClick={()=> {setvalue(value-value); setRight(right-right); setWrong(wrong-wrong)}} className={`${value==39?"invisible":"bg-green-600 text-2xl font-bold w-44 h-10 mt-10 ml-10"} md:invisible visible`}>Start again</button></div>
             
         </div>
-            <div className={`${value!=39?"invisible":"absolute top-16 text-center bg-gray-500 p-10 w-2/3 rounded-3xl h-64 ml-44 mt-32"}`}>
+            <div className={`${value!=39?"invisible":"absolute top-16 text-center bg-gray-500 md:p-10 p-2 md:w-2/3 w-96 rounded-3xl md:h-64 h-96 md:ml-44 m-2 ml-6 md:mt-32"}`}>
             <p className="text-5xl font-bold">Your Score</p>
             <p className="text-3xl text-green-700">total correct : {right}</p>
             <p className="text-3xl text-red-700">Total Wrong : {wrong}</p>
